6405dc464de46449164f7beee4f06b614cd7fd55
ilyvlsmirusaddit <iluka333@yandex.ru>
2025-04-23 12:32:10 +0300

    create dynamic metals table

---
+++ src/Components/DynamicMetals/DynamicMetalsTable.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { DynamicMetalRate } from '../../Models/ApiEntities'
+
+type Props = {
+
+Metals: DynamicMetalRate[]
+
+}
+
+const DynamicMetalsTable = (props: Props) => {
+  return (
+    <div>DynamicMetalsTable</div>
+  )
+}
+
+export default DynamicMetalsTable
